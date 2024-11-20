d0f4df7e42bc58833d72ed7078601cf9bdae83f4
deadlydiamond98 <diamonderrick1@gmail.com>
2024-11-20 03:07:04 -0500

    book screen now has arrows

--- src/client/java/net/deadlydiamond98/familiar_friends/screens/CompanionBookScreen.java
+++ src/client/java/net/deadlydiamond98/familiar_friends/screens/CompanionBookScreen.java
@@ -3,7 +3,9 @@ package net.deadlydiamond98.familiar_friends.screens;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.deadlydiamond98.familiar_friends.FamiliarFriends;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.ingame.BookScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.widget.PageTurnWidget;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
@@ -11,11 +13,54 @@ import net.minecraft.util.Identifier;
 
 public class CompanionBookScreen extends HandledScreen<CompanionBookScreenHandler> {
 
-    private final Identifier BOOK_TEXTURE = Identifier.of(FamiliarFriends.MOD_ID, "textures/gui/companion_book.png");
+    private static final Identifier BOOK_TEXTURE = Identifier.of(FamiliarFriends.MOD_ID, "textures/gui/companion_book.png");
+
+    private int pageIndex;
+    private PageTurnWidget nextPageButton;
+    private PageTurnWidget previousPageButton;
 
     public CompanionBookScreen(CompanionBookScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
     }
+
+    protected void init() {
+        this.addPageButtons();
+    }
+
+    protected void addPageButtons() {
+        int i = (this.width - 320) / 2;
+        this.nextPageButton = this.addDrawableChild(new PageTurnWidget(i + 280, 170, true, (button) -> this.goToNextPage(), true));
+        this.previousPageButton = this.addDrawableChild(new PageTurnWidget(i + 16, 170, false, (button) -> this.goToPreviousPage(), true));
+        this.updatePageButtons();
+    }
+
+    protected void goToPreviousPage() {
+        if (this.pageIndex > 0) {
+            --this.pageIndex;
+        }
+
+        this.updatePageButtons();
+    }
+
+    protected void goToNextPage() {
+        if (this.pageIndex < this.getPageCount() - 1) {
+            ++this.pageIndex;
+        }
+
+        this.updatePageButtons();
+    }
+
+    private void updatePageButtons() {
+        this.nextPageButton.visible = this.pageIndex < this.getPageCount() - 1;
+        this.previousPageButton.visible = this.pageIndex > 0;
+    }
+
+
+    // This Variable is temporary, change when adding way to figure out # of familiars
+    private int getPageCount() {
+        return 100;
+    }
+
 
     @Override
     protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
