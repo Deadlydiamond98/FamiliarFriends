aa659b642b304d448fa1d8b0f775bc7c243c4f2d
deadlydiamond98 <diamonderrick1@gmail.com>
2024-11-26 09:13:16 -0500

    Mobs now have a locked mode in the gui (need to make it toggle off with an unequip still, right now it's always on). Text also centers and stays in the book (unless the text is ungodly long, but that's unlikely to happen... I hope)

--- src/client/java/net/deadlydiamond98/familiar_friends/screens/CompanionBookScreen.java
+++ src/client/java/net/deadlydiamond98/familiar_friends/screens/CompanionBookScreen.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.deadlydiamond98.familiar_friends.FamiliarFriends;
 import net.deadlydiamond98.familiar_friends.entities.PlayerCompanion;
 import net.deadlydiamond98.familiar_friends.util.BookCompanionRegistry;
+import net.deadlydiamond98.familiar_friends.util.TextFormatHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
@@ -31,8 +32,6 @@ public class CompanionBookScreen extends HandledScreen<CompanionBookScreenHandle
 
     private final List<PlayerCompanion> RENDERED_COMPANIONS = new ArrayList<>();
 
-//    private final List<Integer> LETTER_INDEXS = new ArrayList<>();
-
     private int pageIndex;
     private PageTurnWidget nextPageButton;
     private PageTurnWidget previousPageButton;
@@ -43,8 +42,6 @@ public class CompanionBookScreen extends HandledScreen<CompanionBookScreenHandle
 
     protected void init() {
         this.addCompanions();
-        // TODO: find the first of each letter, then implement skipping around companions based on the first letter!
-//        this.createLetterIndex();
         this.addPageButtons();
     }
 
@@ -120,7 +117,7 @@ public class CompanionBookScreen extends HandledScreen<CompanionBookScreenHandle
 
 
     private int getPageCount() {
-        return RENDERED_COMPANIONS.size();
+        return RENDERED_COMPANIONS.size() + 1;
     }
 
     @Override
@@ -139,19 +136,32 @@ public class CompanionBookScreen extends HandledScreen<CompanionBookScreenHandle
             return;
         }
 
+        // If on the first page, render the page selection area rather than an entity
+        if (this.pageIndex == 0) {
+            drawFirstPage(context, delta, mouseX, mouseY, x, y);
+            return;
+        }
+
         // Entity Page
 
-        PlayerCompanion companion = RENDERED_COMPANIONS.get(pageIndex);
+        PlayerCompanion companion = RENDERED_COMPANIONS.get(pageIndex - 1);
 
         int companionX = x - 80;
-        int companionY = y + 85;
+        int companionY = y + 105;
 
-        drawEntity(context, companionX, companionY, 40, 0.0625F, companion); // Entity Model
+        context.drawTexture(BOOK_TEXTURE, ((this.width - 106) / 2) - 80, y + 22, 329, 2, 106, 106, 512, 512); // Entity frame thing
+
+        drawEntity(context, companionX, companionY, 40, 0.0625F, companion, true); // Entity Model
 
         Text name = companion.getName().copy().setStyle(Style.EMPTY.withUnderline(true));
-        drawResizeableCenteredText(context, matrices, companionX, companionY + 20, name, 1.25f, 0x574436); // Entity Name
 
-        drawResizeableCenteredText(context, matrices, companionX, companionY + 35, companion.getAuthor(), 0.55f, 0x95836a); // Entity Author
+        companionY += 25;
+
+        drawResizeableCenteredText(context, matrices, companionX, companionY, name, 1.25f, 0x574436); // Entity Name
+
+        companionY += 15;
+
+        drawResizeableCenteredText(context, matrices, companionX, companionY, companion.getAuthor(), 0.55f, 0x95836a); // Entity Author
 
 
         // Description Page
@@ -161,15 +171,40 @@ public class CompanionBookScreen extends HandledScreen<CompanionBookScreenHandle
 
         Text descriptionTitle = Text.translatable("gui.familiar_friends.desc").setStyle(Style.EMPTY.withUnderline(true));
 
-        drawResizeableCenteredText(context, matrices, descriptionX, descriptionY, descriptionTitle, 1.25f, 0x574436); // Description Title
+        drawResizeableCenteredText(context, matrices, descriptionX, descriptionY, descriptionTitle, 1.0f, 0x574436); // Description Title
 
+        descriptionY += 15;
 
+        drawResizeableCenteredText(context, matrices, descriptionX, descriptionY, companion.getDescription(), 0.75f, 0x766450); // Companion Description
     }
 
     private void drawResizeableCenteredText(DrawContext context, MatrixStack matrices, int companionX, int companionY, Text text, float size, int color) {
-        matrices.push();
 
         float nameLength = ((float) textRenderer.getWidth(text) / 2) * size;
+
+        float maxLength = 25 / size;
+        if (nameLength >= maxLength) {
+            String fullText = text.getString();
+            Style style = text.getStyle();
+
+            int splitIndex = TextFormatHelper.findCommaSplitIndex(fullText, (int) maxLength);
+
+            if (splitIndex == -1) {
+                splitIndex = TextFormatHelper.findSplitIndex(fullText, (int) maxLength);
+            }
+
+            text = Text.literal(fullText.substring(0, splitIndex).trim()).setStyle(style);
+            nameLength = ((float) textRenderer.getWidth(text) / 2) * size;
+
+            String remainingText = fullText.substring(splitIndex).trim();
+            if (remainingText.startsWith(",")) {
+                remainingText = remainingText.substring(1).trim();
+            }
+
+            drawResizeableCenteredText(context, matrices, companionX, companionY + 10,
+                    Text.literal(remainingText).setStyle(style), size, color);
+        }
+        matrices.push();
 
         matrices.translate(companionX - nameLength, companionY, 0);
         matrices.scale(size, size, size);
@@ -179,10 +214,30 @@ public class CompanionBookScreen extends HandledScreen<CompanionBookScreenHandle
         matrices.pop();
     }
 
+    // TODO: add main page for easier navigation!
+    private void drawFirstPage(DrawContext context, float delta, int mouseX, int mouseY, int x, int y) {
+        MatrixStack martices = context.getMatrices();
+        drawResizeableCenteredText(context, martices, x, y + 90, Text.literal("Put Page Select Here Later!"), 1.0f, 0x000000);
+    }
+
+    @Override
+    protected void drawForeground(DrawContext context, int mouseX, int mouseY) {
+        if (this.pageIndex == 0) {
+            return;
+        }
+
+        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
+        RenderSystem.setShaderTexture(0, BOOK_TEXTURE);
+
+        //        PlayerCompanion companion = RENDERED_COMPANIONS.get(pageIndex - 1);
+
+        context.drawTexture(BOOK_TEXTURE, ((this.width - 106) / 2) - 82, 22, 110, 110, 57, 204, 56, 56, 512, 512);
+    }
+
 
     // These are modified versions of the drawEntity methods from the inventory
 
-    public static void drawEntity(DrawContext context, int x, int y, int size, float f, PlayerCompanion entity) {
+    public static void drawEntity(DrawContext context, int x, int y, int size, float f, PlayerCompanion entity, boolean locked) {
         float rotationAngle = (float)(System.currentTimeMillis() % 10000l) / 10000.0f * 360.0f;
         float bobbingOffset = (float)Math.sin(System.currentTimeMillis() / 500.0) * 0.1f;
 
@@ -201,7 +256,16 @@ public class CompanionBookScreen extends HandledScreen<CompanionBookScreenHandle
         entity.prevHeadYaw = entity.getYaw();
 
         Vector3f vector3f = new Vector3f(0.0f, entity.getHeight() / 2.0f + f + bobbingOffset, 0.0f);
+
+        if (locked) {
+            RenderSystem.setShaderColor(0.0f, 0.0f, 0.0f, 1.0f);
+        } else {
+            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+
         drawEntity(context, x, y, size, vector3f, rotationQuaternion, null, entity);
+
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
 
         entity.bodyYaw = bodyYawBackup;
         entity.setYaw(yawBackup);
@@ -230,11 +294,5 @@ public class CompanionBookScreen extends HandledScreen<CompanionBookScreenHandle
         entityRenderDispatcher.setRenderShadows(true);
         context.getMatrices().pop();
         DiffuseLighting.enableGuiDepthLighting();
-    }
-
-
-    //removed super to make empty, did this to remove the "inventory" Text, since there is no Inventory
-    @Override
-    protected void drawForeground(DrawContext context, int mouseX, int mouseY) {
     }
 }
